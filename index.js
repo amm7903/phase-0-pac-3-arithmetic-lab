17a7aee859fc9ef92100a84bfdb3369979336666
Alejandro Marcano <+>
2021-09-11 22:42:50 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,44 @@
+//add a and b
+function add(a,b){
+return a + b;  }
+
+//subtract a and b
+function subtract(a,b){
+    return a - b;
+}
+
+//multiply a and b
+function multiply(a,b){
+    return a * b;
+}
+
+//divide a and b 
+function divide(a,b){
+    return a / b;
+}
+
+//increment up by 1
+function increment(a){
+    return ++a;
+}
+//decreases by 1
+function decrement(a){
+    return --a;
+}
+
+//makes int to string
+function makeInt(a){
+    return a;
+}
+
+
+
+function preserveDecimal(a){
+    return a;
+}
+
+
+//make string to number using parse
+parseInt("a", 10);
+    
+parseFloat(a)
